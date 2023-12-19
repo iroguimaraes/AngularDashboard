181f48427b5a8c159c54ef6522970dcf22187e8e
Italo Rafael <betaionly@gmail.com>
2023-12-19 19:53:31 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterOutlet, RouterLink, RouterLinkActive, provideRouter } from '@angular/router';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { HomeComponent } from './home/home.component';
+import { TestDashboardComponent } from './test-dashboard/test-dashboard.component';
+import { MatMenuModule } from '@angular/material/menu';
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    TestDashboardComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
