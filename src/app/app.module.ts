2af70023782bf382e35568838e6b5e48e9717682
Italo Rafael <betaionly@gmail.com>
2023-12-29 17:01:10 -0300

    made a table with paginators that work properly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 // Router Module
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { RouterOutlet, RouterLink, RouterLinkActive, provideRouter } from '@angular/router';
 
 // Material Module
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 
 // Components
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MatMenuModule } from '@angular/material/menu';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RelatoriosComponent } from './relatorios/relatorios.component';
 
 // PrimeNG Chart Module
 import { ChartModule } from 'primeng/chart';
@@ -28,12 +33,15 @@ import { ChartModule } from 'primeng/chart';
   declarations: [
     AppComponent,
     DashboardComponent,
-    HomeComponent
+    HomeComponent,
+    RelatoriosComponent,
   ],
   imports: [
     // Essential
     CommonModule,
     BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
     // Routing
     AppRoutingModule,
     // Material Modules
@@ -46,10 +54,12 @@ import { ChartModule } from 'primeng/chart';
     RouterLink,
     RouterLinkActive,
     MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
     // PrimeNG Chart Modules
     ChartModule
   ],
-  providers: [],
+  providers: [MatPaginator, MatTableDataSource],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
