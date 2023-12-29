3b783219f3c1dd5c2444e7c9b0f5e8b85917c4f7
Italo Rafael <betaionly@gmail.com>
2023-12-29 15:52:33 -0300

    added router to relatorios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { RelatoriosComponent } from './relatorios/relatorios.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'relatorios', component: RelatoriosComponent },
 ];
 
 @NgModule({
